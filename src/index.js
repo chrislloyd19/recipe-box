8732aeeda5ceac05da0369738ca496553b6af572
chris lloyd <dev@chrislloyd.me>
2017-02-16 21:15:33 -0500

    move around state

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import { FormControl } from 'react-bootstrap';
 
 class Recipe extends React.Component {
   render() {
-    let ingredients = this.props.ingredients.map(function(ingredient, index) {
+    let ingredients = this.props.recipe.ingredients.map(function(ingredient, index) {
       return (
         <ListGroupItem key={index}>{ingredient}</ListGroupItem>
       )
@@ -23,7 +23,7 @@ class Recipe extends React.Component {
       <div>
         <Panel
           bsStyle="info"
-          header={this.props.name}
+          header={this.props.recipe.name}
           collapsible>
             <h3 className="text-center">Ingredients</h3>
             <hr/>
@@ -37,21 +37,38 @@ class Recipe extends React.Component {
 }
 
 class RecipeBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {recipes: []};
+
+    this.save = this.save.bind(this);
+  }
+  componentWillMount() {
+    let recipes = JSON.parse(localStorage.getItem('recipes')) || [];
+    this.setState({recipes: recipes});
+  }
+  save(name, ingredients) {
+    if(name && ingredients) {
+      let newRecipe = {name:name, ingredients:ingredients.split(',')};
+      let newRecipes = this.state.recipes.concat([newRecipe]);
+
+      this.setState({recipes: newRecipes});
+      localStorage.setItem('recipes', JSON.stringify(newRecipes));
+    }
+  }
   render() {
-    let recipes = this.props.recipes.map(function(recipe, index) {
+    let recipes = this.state.recipes.map(function(recipe, index) {
         return (
           <Recipe
             key={index}
-            name={recipe.name}
-            ingredients={recipe.ingredients} />
+            recipe={recipe} />
         )
     });
     return (
       <Well>
         {recipes}
         <AddRecipe
-          save={this.props.save} updateNewRecipeName={this.props.updateNewRecipeName}
-          updateNewIngredients={this.props.updateNewIngredients}/>
+          save={this.save} />
       </Well>
     )
   }
@@ -60,10 +77,12 @@ class RecipeBox extends React.Component {
 class AddRecipe extends React.Component {
   constructor(props){
     super(props);
-    this.state = {showModal: false};
+    this.state = {showModal: false, recipeName: '', ingredients: ''};
 
     this.close = this.close.bind(this);
     this.open = this.open.bind(this);
+    this.updateRecipeName = this.updateRecipeName.bind(this);
+    this.updateIngredients = this.updateIngredients.bind(this);
   }
 
   close() {
@@ -74,15 +93,20 @@ class AddRecipe extends React.Component {
     this.setState({ showModal: true });
   }
 
+  updateRecipeName(e) {
+    this.setState({recipeName: e.target.value});
+  }
+
+  updateIngredients(e) {
+    this.setState({ingredients: e.target.value});
+  }
+
   render() {
     return (
       <div>
         <Button
           bsStyle="primary"
-          onClick={this.open}
-        >
-          Add recipe
-        </Button>
+          onClick={this.open}> Add recipe </Button>
 
         <Modal show={this.state.showModal} onHide={this.close}>
           <Modal.Header closeButton>
@@ -95,30 +119,26 @@ class AddRecipe extends React.Component {
                 <FormControl
                   type="text"
                   placeholder="Recipe Name"
-                  onChange={this.props.updateNewRecipeName}
-                />
+                  onChange={this.updateRecipeName}
+                  value={this.state.recipeName} />
               </FormGroup>
               <FormGroup>
                 <ControlLabel>Ingredients</ControlLabel>
                 <FormControl
                   componentClass="textarea"
                   placeholder="Enter ingredients separated,by,commas"
-                  onChange={this.props.updateNewIngredients}
-                />
+                  onChange={this.updateIngredients}
+                  value={this.state.ingredients} />
               </FormGroup>
             </form>
           </Modal.Body>
           <Modal.Footer>
             <Button
               bsStyle="primary"
-              onClick={() => {this.props.save(); this.close();}}
-            >
-              Save
+              onClick={() => {this.props.save(this.state.recipeName, this.state.ingredients); this.close();}} > Save
             </Button>
             <Button
-              onClick={this.close}
-            >
-                Close
+              onClick={this.close} > Close
             </Button>
           </Modal.Footer>
         </Modal>
@@ -128,41 +148,9 @@ class AddRecipe extends React.Component {
 }
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {recipes: []};
-
-    this.save = this.save.bind(this);
-    this.updateNewRecipeName = this.updateNewRecipeName.bind(this);
-    this.updateNewIngredients = this.updateNewIngredients.bind(this);
-  }
-  componentWillMount() {
-    let recipes = JSON.parse(localStorage.getItem('recipes')) || [];
-    this.setState({recipes: recipes});
-  }
-  save() {
-    if(this.state.recipeName && this.state.ingredients) {
-      let newIngredients = this.state.ingredients.split(',');
-      let newRecipes = this.state.recipes.concat([{name: this.state.recipeName, ingredients: newIngredients}]);
-
-      this.setState({recipes: newRecipes});
-      localStorage.setItem('recipes', JSON.stringify(newRecipes));
-    }
-  }
-  updateNewRecipeName(e) {
-    this.setState({recipeName: e.target.value});
-  }
-  updateNewIngredients(e) {
-    this.setState({ingredients: e.target.value});
-  }
   render() {
     return (
-      <RecipeBox
-        recipes={this.state.recipes}
-        save={this.save}
-        updateNewRecipeName={this.updateNewRecipeName}
-        updateNewIngredients={this.updateNewIngredients}
-        />
+      <RecipeBox />
     )
   }
 }
